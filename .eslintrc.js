3e36b39197fcf7ced723af821613285677be8b4a
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2021-02-24 18:49:11 -0400

    v1.5.0 - update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
-  extends: ['airbnb', 'prettier', 'prettier/prettier', 'prettier/react'],
-  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier'],
+  plugins: ['html', 'prettier', 'react-hooks'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
-    ecmaVersion: 2020,
+    requireConfigFile: false,
+    ecmaVersion: 2021,
     ecmaFeatures: {
       impliedStrict: true,
-      classes: true,
+      jsx: true,
     },
   },
   env: {
@@ -50,29 +52,12 @@ module.exports = {
       },
     ],
     'no-console': 0,
-    'import/prefer-default-export': 0,
-    import: 0,
     'func-names': 0,
     'space-before-function-paren': 0,
     'comma-dangle': 0,
     'max-len': 0,
-    'import/extensions': 0,
     'no-underscore-dangle': 0,
     'consistent-return': 0,
-    'react/display-name': 1,
-    'react/no-array-index-key': 0,
-    'react/react-in-jsx-scope': 0,
-    'react/prefer-stateless-function': 0,
-    'react/forbid-prop-types': 0,
-    'react/no-unescaped-entities': 0,
-    'jsx-a11y/accessible-emoji': 0,
-    'react/require-default-props': 0,
-    'react/jsx-filename-extension': [
-      1,
-      {
-        extensions: ['.js', '.jsx'],
-      },
-    ],
     radix: 0,
     'no-shadow': [
       2,
@@ -89,6 +74,24 @@ module.exports = {
         allowTemplateLiterals: true,
       },
     ],
+    import: 0,
+    'import/prefer-default-export': 0,
+    'import/extensions': 0,
+    'react/display-name': 1,
+    'react/no-array-index-key': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prefer-stateless-function': 0,
+    'react/forbid-prop-types': 0,
+    'react/no-unescaped-entities': 0,
+    'react/require-default-props': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'prettier/prettier': [
       'error',
       {
@@ -101,6 +104,7 @@ module.exports = {
         useTabs: false,
       },
     ],
+    'jsx-a11y/accessible-emoji': 0,
     'jsx-a11y/href-no-hash': 'off',
     'jsx-a11y/anchor-is-valid': [
       'warn',
@@ -108,8 +112,5 @@ module.exports = {
         aspects: ['invalidHref'],
       },
     ],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
   },
-  plugins: ['html', 'prettier', 'react-hooks'],
 };
